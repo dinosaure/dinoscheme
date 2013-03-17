1061c30d3e198bd01260d87297880f45e9679d67
Romain Calascibetta <romain.calascibetta@epitech.eu>
2013-03-17 23:46:09 +0100

    Remove DEBUG mode

--- dini/include/dinoscheme.h
+++ dini/include/dinoscheme.h
@@ -19,39 +19,4 @@
 
 typedef int32_t case_t;
 
-struct pgrm_s
-{
-  /*
-   * MEMORY HEAP
-   *
-   */
-
-  case_t  memory[SIZE_MEMORY];
-  case_t  newspace;
-  case_t  oldspace;
-  case_t  ts;
-
-  /*
-   * REGISTERS
-   *
-   */
-
-  case_t  registers[SIZE_REGISTER];
-
-  /*
-   * STACK
-   *
-   */
-
-  case_t  rs[SIZE_STACK];
-
-  /*
-   * CODE
-   *
-   */
-
-  char    *instruction;
-};
-typedef struct pgrm_s pgrm_t;
-
 #endif
